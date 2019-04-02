c04736584bca94bc68dfe738fa42e2318b14958c
姚超臣 <yaochaochen@ctv.org.cn>
2019-04-02 09:59:53 +0800

    局部变量var 的使用 在jdk11

---
+++ src/main/java/jdk12/Lambda.java
@@ -0,0 +1,16 @@
+package jdk12;
+
+public class Lambda {
+    
+    //var  x = 1;
+    //var n;//在使用var类型的时候，必须实例化 否则无法推断类型 必须作用于局部变量
+    //var s =1;
+    //s= "11"; 当var赋值中 在赋值就也不行
+    
+   // var s = new A();
+    
+    public  class A {
+        
+    }
+        
+}
